e2463c5dbe27e62ed637b4ad777ffadb9f30e18d
Oliver Kroener - IT for a better life! <ok@oliver-kroener.de>
2024-07-31 21:54:29 +0200

    Changes for languages

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,17 +1,17 @@
 <?php
 
-defined('TYPO3') or die();
+// defined('TYPO3') or die();
 
 call_user_func(function () {
         \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-            'OliverKroener.ok_azure_login',
+            'OkAzureLogin',
             'Pi1',
             [
-                \OliverKroener\OkAzureLogin\Controller\AzureLoginController::class => 'login'
+                'AzureLoginController' => 'login'
             ],
             // non-cacheable actions
             [
-                \OliverKroener\OkAzureLogin\Controller\AzureLoginController::class => 'login'
+                'AzureLoginController' => 'login'
             ]
         );
     }
